b770ec140496b13cbff82700b9ef10ed768b6d4f
gregladden <greg.ladden@g.austincc.edu>
2024-04-03 20:48:48 -0500

    new keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,4 +7,4 @@ keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 keymap.set('n', '<leader>sv', '<C-w>v')
 keymap.set('n', '<leader>sh', '<C-w>s')     -- Split window horizontally
 keymap.set('n', '<leader>se', '<C-w>=')     -- Make split windows equal with
-keymap.set('n', '<leader>sx', ':close<CR>') -- Closevurrent split windoh
+keymap.set('n', '<leader>sx', ':close<CR>') -- Close current window
