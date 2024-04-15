d87c1040ca252d72283c72df01b39e9d22a17b66
gregladden <greg.ladden@g.austincc.edu>
2024-04-15 17:23:27 -0500

    theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,12 +7,15 @@ local M = {
 function M.config()
   local everforest = require("everforest")
   everforest.setup({
-    background = "hard",
+    background = "medium",
     transparent_background_level = 0,
     italics = true,
     disable_italic_comments = false,
     on_highlights = function(hl, _)
-      hl["@string.special.symbol.ruby"] = { link = "@field" }
+      hl.TelescopeBorder = { link = "Orange" }
+      hl.TSKeywordFunction = { link = "Green" }
+      hl.TSMethod = { link = "Green" }
+      hl["@lsp.type.class"] = { link = "Blue" }
     end,
   })
   everforest.load()
