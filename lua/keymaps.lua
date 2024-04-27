136f0e222ba1d9f91c15d1a9a296e16402d4a7de
gregladden <greg.ladden@g.austincc.edu>
2024-04-27 01:48:49 -0500

    color updates

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,16 +1,36 @@
 local keymap = vim.keymap
 
 -- Removes highlighting
-keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- Split window horizontally
-keymap.set('n', '<leader>wv', '<C-w>v', { desc = "[w]indow [v]ertical" })
-keymap.set('n', '<leader>wh', '<C-w>s', { desc = "[w]indow [h]orizontal" })
-keymap.set('n', '<leader>we', '<C-w>=', { desc = "[w]indow [e]qual size" })
-keymap.set('n', '<leader>wc', ':close<CR>', { desc = "[w]indow [c]lose" })
+keymap.set("n", "<leader>wv", "<C-w>v", { desc = "[w]indow [v]ertical" })
+keymap.set("n", "<leader>wh", "<C-w>s", { desc = "[w]indow [h]orizontal" })
+keymap.set("n", "<leader>we", "<C-w>=", { desc = "[w]indow [e]qual size" })
+keymap.set("n", "<leader>wc", ":close<CR>", { desc = "[w]indow [c]lose" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "[t]ab [o]pen new" })
 keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "[t]ab [c]lose current" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "[t]ab [n]ext" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "[t]ab [p]revious" })
 keymap.set("n", "<leader>tb", "<cmd>tabnew %<CR>", { desc = "[t]ab [b]buffer - Copy buffer to new tab " })
+
+-- Fold and unfold mappings
+keymap.set("n", "<space>ft", "za", { noremap = true, desc = "[f]old [t]oggle line under cursor" }) -- Toggle fold under cursor
+keymap.set("n", "<space>fo", "zR", { noremap = true, desc = "[f]old [o]pen all" }) -- Open all folds
+keymap.set("n", "<space>fc", "zM", { noremap = true, desc = "[f]old [c]lose all" }) -- Close all folds
+-- Toggle foldmethod between indent and manual
+keymap.set(
+	"n",
+	"<leader>fs",
+	":lua ToggleFoldmethod()<CR>",
+	{ noremap = true, silent = true, desc = "[f]old [s]et Toggles set fold to manual or indent" }
+)
+
+function ToggleFoldmethod()
+	if vim.o.foldmethod == "indent" then
+		vim.o.foldmethod = "manual"
+	else
+		vim.o.foldmethod = "indent"
+	end
+end
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,24 +1,42 @@
 local M = {
-  "neanias/everforest-nvim",
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
+	"neanias/everforest-nvim",
+	lazy = false,
+	priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  local everforest = require("everforest")
-  everforest.setup({
-    background = "medium",
-    transparent_background_level = 0,
-    italics = true,
-    disable_italic_comments = false,
-    on_highlights = function(hl, _)
-      hl.TelescopeBorder = { link = "Orange" }
-      hl.TSKeywordFunction = { link = "Green" }
-      hl.TSMethod = { link = "Green" }
-      hl["@lsp.type.class"] = { link = "Blue" }
-    end,
-  })
-  everforest.load()
+	local everforest = require("everforest")
+	everforest.setup({
+		background = "hard",
+		transparent_background_level = 0,
+		italics = true,
+		disable_italic_comments = false,
+		on_highlights = function(hl, _)
+			hl.TelescopeBorder = { link = "Blue" }
+			hl.TSKeywordFunction = { link = "Green" }
+			-- hl.TSMethod = { link = "Green" }
+			-- hl.TSFunction = { link = "Green" }
+			-- hl.TSField = { link = "Green" }
+			-- hl.TSProperty = { link = "Green" }
+			hl.TSPunctSpecial = { link = "Green" }
+			hl.TSKeyword = { link = "Green" }
+			hl.TSConditional = { link = "Orange" }
+			hl.TSKeywordReturn = { link = "Orange" }
+			hl.TSRepeat = { link = "Orange" }
+			hl["@lsp.type.class"] = { link = "Green" }
+		end,
+		colours_override = function(palette)
+			palette.orange = "#FAAF4A"
+			palette.red = "#F69092"
+			palette.aqua = "#E9D4A8"
+			palette.blue = "#8DB5BC"
+			palette.purple = "#C7A4E0"
+			palette.green = "#92BF8D"
+			palette.yellow = "#E8EF80"
+			-- palette.fg = "#DDCDAB"
+		end,
+	})
+	everforest.load()
 end
 
 return M
