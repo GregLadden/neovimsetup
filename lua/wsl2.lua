f439fcef4fe9c90599fed9948aa30d3c71f1d570
Greg_Ladden <greg.ladden@dell.com>
2024-05-17 16:13:59 -0500

    wsl2

--- lua/wsl2.lua
+++ lua/wsl2.lua
@@ -1,14 +1,14 @@
 -- Sync clipboard for WSL2 on Windows
 -- Uncomment for WSL2
-vim.g.clipboard = {
-  name = 'xclip',
-  copy = {
-    ['+'] = 'xclip -selection clipboard -in',
-    ['*'] = 'xclip -selection primary -in',
-  },
-  paste = {
-    ['+'] = 'xclip -selection clipboard -out',
-    ['*'] = 'xclip -selection primary -out',
-  },
-  cache_enabled = 1,
-}
+-- vim.g.clipboard = {
+--   name = 'xclip',
+--   copy = {
+--     ['+'] = 'xclip -selection clipboard -in',
+--     ['*'] = 'xclip -selection primary -in',
+--   },
+--   paste = {
+--     ['+'] = 'xclip -selection clipboard -out',
+--     ['*'] = 'xclip -selection primary -out',
+--   },
+--   cache_enabled = 1,
+-- }
