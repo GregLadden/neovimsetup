9b001f4e0eeaacc45f79fd475eddb390644713a5
GregLadden <gregory.ladden@gmail.com>
2024-10-23 01:09:44 -0500

    updates

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,73 +1,69 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    -- import mason-tool-installer
-    local mason_tool_installer = require("mason-tool-installer")
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- import mason-tool-installer
+		local mason_tool_installer = require("mason-tool-installer")
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "pyright",
-        "clangd",
-        "dockerls",
-        "docker_compose_language_service",
-        "gopls",
-        "templ",
-        "yamlls",
-      },
-    })
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "black",
-        "debugpy",
-        "flake8",
-        "isort",
-        "mypy",
-        "pylint",
-      },
-    })
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"pyright",
+				"clangd",
+				"dockerls",
+				"docker_compose_language_service",
+				"gopls",
+				"templ",
+				"yamlls",
+			},
+		})
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"debugpy",
+				"ruff",
+			},
+		})
 
-    local lspconfig = require('lspconfig')
-    local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-    local lsp_attach = function(client, bufnr)
-      -- Create your keybindings here...
-    end
+		local lspconfig = require("lspconfig")
+		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lsp_attach = function(client, bufnr)
+			-- Create your keybindings here...
+		end
 
-    -- Call setup on each LSP server
-    require('mason-lspconfig').setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({
-          on_attach = lsp_attach,
-          capabilities = lsp_capabilities,
-        })
-      end
-    })
-  end,
+		-- Call setup on each LSP server
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				lspconfig[server_name].setup({
+					on_attach = lsp_attach,
+					capabilities = lsp_capabilities,
+				})
+			end,
+		})
+	end,
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,46 +1,46 @@
 local M = {
-  "neanias/everforest-nvim",
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
+	"neanias/everforest-nvim",
+	lazy = false,
+	priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 function M.config()
-  local everforest = require("everforest")
-  everforest.setup({
-    background = "hard",
-    transparent_background_level = 0,
-    italics = true,
-    disable_italic_comments = false,
-    on_highlights = function(hl, _)
-      hl.TelescopeBorder = { link = "Blue" }
-      hl.TSKeywordFunction = { link = "Green" }
-      -- hl.TSMethod = { link = "Green" }
-      -- hl.TSFunction = { link = "Green" }
-      -- hl.TSField = { link = "Green" }
-      -- hl.TSProperty = { link = "Green" }
-      hl.TSPunctSpecial = { link = "Green" }
-      hl.TSKeyword = { link = "Green" }
-      hl.TSConditional = { link = "Orange" }
-      hl.TSKeywordReturn = { link = "Orange" }
-      hl.TSRepeat = { link = "Orange" }
-      hl.TSInclude = { link = "Green" }
-      hl.javascriptTSInclude = { link = "Green" }
-      hl.goTSInclude = { link = "Green" }
-      hl["@lsp.type.class"] = { link = "Green" }
-    end,
-    colours_override = function(palette)
-      palette.orange = "#FAAF4A"
-      palette.red = "#E67E80"
-      palette.aqua = "#FBE6BC"
-      palette.blue = "#8DB5BC"
-      palette.purple = "#C7A4E0"
-      palette.green = "#92BF8D"
-      palette.yellow = "#E8EF80"
-      palette.fg = "#FBE6BC"
-      palette.bg0 = "#232A2E"
-    end,
-  })
-  everforest.load()
+	local everforest = require("everforest")
+	everforest.setup({
+		background = "hard",
+		-- transparent_background_level = 2,
+		italics = true,
+		disable_italic_comments = false,
+		on_highlights = function(hl, _)
+			hl.TelescopeBorder = { link = "Blue" }
+			hl.TSKeywordFunction = { link = "Green" }
+			-- hl.TSMethod = { link = "Green" }
+			-- hl.TSFunction = { link = "Green" }
+			-- hl.TSField = { link = "Green" }
+			-- hl.TSProperty = { link = "Green" }
+			hl.TSPunctSpecial = { link = "Green" }
+			hl.TSKeyword = { link = "Green" }
+			hl.TSConditional = { link = "Orange" }
+			hl.TSKeywordReturn = { link = "Orange" }
+			hl.TSRepeat = { link = "Orange" }
+			hl.TSInclude = { link = "Green" }
+			hl.javascriptTSInclude = { link = "Green" }
+			hl.goTSInclude = { link = "Green" }
+			hl["@lsp.type.class"] = { link = "Green" }
+		end,
+		colours_override = function(palette)
+			palette.orange = "#FAAF4A"
+			palette.red = "#E67E80"
+			palette.aqua = "#FBE6BC"
+			palette.blue = "#8DB5BC"
+			palette.purple = "#C7A4E0"
+			palette.green = "#92BF8D"
+			palette.yellow = "#E8EF80"
+			palette.fg = "#FBE6BC"
+			palette.bg0 = "#232A2E"
+		end,
+	})
+	everforest.load()
 end
 
 return M
